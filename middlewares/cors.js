f3cb14c0322f4d84cb5a4c7c31b36627b5ca0f1c
kuchikimode <149279076+kuchikimode@users.noreply.github.com>
2024-05-30 14:08:59 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,19 +1,21 @@
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000',
-    'localhost:3001'
-];
-
-function cors(req, res, next) {
+    "https://bankai-frontend.nomoredomainswork.ru",
+    "http://localhost:3001",
+    "http://localhost:3000",
+  ];
+  
+  function cors(req, res, next) {
     const { origin } = req.headers;
-
+  
     if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
+      res.header("Access-Control-Allow-Origin", origin);
     }
     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    );
     next();
-}
-
-module.exports = cors;
+  }
+  
+  module.exports = cors;
